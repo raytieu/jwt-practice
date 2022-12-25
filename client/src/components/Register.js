f1841b6d04eb76b70a956d23c68b3d5e982d1e4c
raytieu <ray.tieu@gmail.com>
2022-12-25 15:51:37 -0800

    set up login component

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = ({ setAuth }) => {
   const [inputs, setInputs] = useState({
@@ -61,6 +62,7 @@ const Register = ({ setAuth }) => {
         />
         <button className="btn btn-success btn-block">Submit</button>
       </form>
+      <Link to="/login">Login</Link>
     </Fragment>
   );
 };
